772c4286363f42a262c45b3784fdce22618ae8fb
code-reaper08 <v.i.s.h.w.a.08.11.2001@gmail.com>
2021-06-29 23:58:13 +0530

    Prettified the code

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const DATA = require('./DATA.json');
+const DATA = require("./DATA.json");
 let PORT = process.env.PORT || 3000;
 
-app.get('/',(req,res) => {
-    res.send("HELLO I AM RUNNING....");
+// Home route
+app.get("/", (req, res) => {
+  res.send("HELLO I AM RUNNING....");
 });
 
-app.get('/:name',(req,res) => {
-    try{
-        DATA.laureates.forEach(laureate => {
-            const fname = laureate.firstname;
-            const sname = laureate.surname;
-            const fullname = fname.concat(sname).toUpperCase();
-            const usrqryname = req.params.name.toUpperCase();
-            if (usrqryname == fullname) {
-                res.status(200).send(laureate)
-                console.log("Can do shit")
-            }
-        });
-    }catch(err) {
-        res.status(400).json({message : err.message});
-    }
-    
-    
-})
+// get data with name
+app.get("/:name", (req, res) => {
+  try {
+    DATA.laureates.forEach((laureate) => {
+      const fname = laureate.firstname;
+      const sname = laureate.surname;
+      const fullname = fname.concat(sname).toUpperCase();
+      const usrqryname = req.params.name.toUpperCase();
+      if (usrqryname == fullname) {
+        res.status(200).send(laureate);
+        console.log("Can do shit");
+      }
+    });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
-app.listen(PORT, () =>{
-    console.log(`server running on http://localhost:${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`server running on http://localhost:${PORT}`);
+});
