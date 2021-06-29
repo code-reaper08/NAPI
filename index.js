f7475f89dadd8fb411c300e484fb87ce6b46de6c
code-reaper08 <v.i.s.h.w.a.08.11.2001@gmail.com>
2021-06-29 23:35:52 +0530

    initial server file with two routes added

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const DATA = require('./DATA.json');
+let PORT = process.env.PORT || 3000;
+
+app.get('/',(req,res) => {
+    res.send("HELLO I AM RUNNING....");
+});
+
+app.get('/:name',(req,res) => {
+    try{
+        DATA.laureates.forEach(laureate => {
+            const nameof = laureate.firstname;
+            if (req.params.name == nameof) {
+                res.status(200).send(laureate)
+                console.log("Can do shit")
+            }
+        });
+    }catch(err) {
+        res.status(400).json({message : err.message});
+    }
+    
+    
+})
+
+app.listen(PORT, () =>{
+    console.log(`server running on http://localhost:${PORT}`);
+})
